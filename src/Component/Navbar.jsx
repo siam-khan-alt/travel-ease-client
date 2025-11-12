6e7c05863733a4544d9feb35382fd6041a249ced
siam-khan-alt <nssiam99@gmail.com>
2025-11-12 08:34:12 +0600

    theme toggle problem solve

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -110,7 +110,8 @@ const Navbar = () => {
         <ul className="menu menu-horizontal px-1 space-x-6">{allLinks}</ul>
       </div>
       <label  className="toggle text-base-content md::ml-5 md:mr-0 mr-7">
-  <input onChange={(e)=>handletheme(e.target.checked)} type="checkbox" value="synthwave" className="theme-controller" />
+  <input onChange={(e)=>handletheme(e.target.checked)} type="checkbox" value="synthwave"
+   checked={theme === "dark"} className="theme-controller" />
 
   <svg aria-label="sun" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor"><circle cx="12" cy="12" r="4"></circle><path d="M12 2v2"></path><path d="M12 20v2"></path><path d="m4.93 4.93 1.41 1.41"></path><path d="m17.66 17.66 1.41 1.41"></path><path d="M2 12h2"></path><path d="M20 12h2"></path><path d="m6.34 17.66-1.41 1.41"></path><path d="m19.07 4.93-1.41 1.41"></path></g></svg>
 
